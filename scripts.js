427072baf180ec75bb4326c683d77a3bfa2a9ef5
jericejanicae <126442695+jericejanicae@users.noreply.github.com>
2023-04-02 14:56:29 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,17 +5,17 @@ function loginValidate () {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if ((username=="") && (password=="")) {
+    if ((username == "") && (password == "")) {
         document.getElementById("alert").innerHTML = "Please enter your username and password";
         document.getElementById("asterisk-u").style.display = "inline";
         document.getElementById("asterisk-p").style.display = "inline";
         alert("Login error.");
-    } else if ((username=="") && (password!="")) {
+    } else if ((username == "") && (password != "")) {
         document.getElementById("alert").innerHTML = "Please enter your username.";
         document.getElementById("asterisk-u").style.display = "inline";
         document.getElementById("asterisk-p").style.display = "none";
         alert("Login error.");
-    } else if ((username!="") && (password=="")) {
+    } else if ((username != "") && (password == "")) {
         document.getElementById("alert").innerHTML = "Please enter your password";
         document.getElementById("asterisk-u").style.display = "none";
         document.getElementById("asterisk-p").style.display = "inline";
